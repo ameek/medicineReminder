0ab9bc455de5707cc347dce7bfe55dd326744178
ameek <ameek.aziz@gmail.com>
2018-08-01 22:34:09 +0600

    test added and model added for bucketList

--- medicine_reminder/api/models.py
+++ medicine_reminder/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bucketlist(models.Model):
+    """This class represnts the bucketlist model"""
+    name = models.CharField(max_length=225, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """return a human readable represntation o"f the model in instance"""
+        return "{}".format(self.name)
