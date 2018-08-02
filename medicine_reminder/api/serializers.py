7cea67d42a3c8c0f92655761b9b8e5f25346cc3b
ameek <ameek.aziz@gmail.com>
2018-08-02 14:04:50 +0600

    its working

--- medicine_reminder/api/serializers.py
+++ medicine_reminder/api/serializers.py
@@ -3,8 +3,9 @@ from .models import Bucketlist
 
 class BucketlistSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
+
     class Meta:
-        """this meta clss will map serializer's feilds with the model feilds"""
+        """Meta class to map serializer's fields with the model fields."""
         model = Bucketlist
-        feilds = ('id','name','date_created','date_modified')
-        read_only_feilds = ('date_created', 'date_modified')
+        fields = ('id', 'name', 'date_created', 'date_modified')
+        read_only_fields = ('date_created', 'date_modified')
