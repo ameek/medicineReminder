781fb9fc186f38226aa9febde9bbe3b1e64cf960
ameek <ameek.aziz@gmail.com>
2018-08-02 13:56:30 +0600

    bug testing

--- medicine_reminder/api/tests.py
+++ medicine_reminder/api/tests.py
@@ -24,15 +24,17 @@ class ModelTestCase(TestCase):
         self.assertNotEqual(old_count, new_count)
 
 class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
     def setUp(self):
-        "define the tet client and other test variables"
+        """Define the test client and other test variables."""
         self.client = APIClient()
-        self.bucketlist_data = {'name': "Go to Ibiza"}
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
         self.response = self.client.post(
             reverse('create'),
-            self.bucketlist_data
-            format="Json"
-        )
+            self.bucketlist_data,
+            format="json")
+
     def test_api_can_create_a_bucketlist(self):
         """Test the api has bucket creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- medicine_reminder/api/views.py
+++ medicine_reminder/api/views.py
@@ -1,13 +1,12 @@
-#from django.shortcuts import render
-
 from rest_framework import generics
 from .serializers import BucketlistSerializer
 from .models import Bucketlist
 
-# Create your views here.
-Class CreateView(generics.ListCreateAPIView):
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
 
-    def perform_create(self, serializers):
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
         serializer.save()
