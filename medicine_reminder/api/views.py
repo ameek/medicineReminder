235cdc1ad87d3ce5b23dff99556b9582ada394d0
ameek <ameek.aziz@gmail.com>
2018-08-02 13:41:33 +0600

    test view and view done

--- medicine_reminder/api/views.py
+++ medicine_reminder/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
 
 # Create your views here.
+Class CreateView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializers):
+        serializer.save()
